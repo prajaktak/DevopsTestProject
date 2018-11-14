8b0804136f6da7d8b78bfe13d8c69a994bcef3b5
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-11-14 15:09:02 +0100

    Initial Commit

---
+++ DevopsTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevopsTestProject
+//
+//  Created by Prajakta Kulkarni on 14/11/2018.
+//  Copyright © 2018 Prajakta Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
